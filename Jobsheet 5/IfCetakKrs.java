ee07c50d8c9d36c6834f2c53ef7712a7155061c9
afivamay <afivamay14@gmail.com>
2025-10-02 10:25:37 +0700

    Tugas 1

--- Jobsheet 5/IfCetakKrs.java
+++ Jobsheet 5/IfCetakKrs.java
@@ -8,12 +8,16 @@ public class IfCetakKrs {
         System.out.println("Apakah UKT lunas? ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silahkan bayar UKT terlebih dahulu");
-        }
+        String pesan;
+        pesan = (uktLunas == true) ? "Silahkan cetak KRS" : "Silahkan bayar UKT terlebih dahulu";
+        System.out.println(pesan);
+
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. Silahkan bayar UKT terlebih dahulu");
+        // }
 
         sc.close();
     }
