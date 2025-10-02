f8df9a74ead41d0d2d5093f9bdfa1ad894645328
afivamay <afivamay14@gmail.com>
2025-10-02 07:38:30 +0700

    Modifikasi percobaan 1

--- Jobsheet 5/IfCetakKrs.java
+++ Jobsheet 5/IfCetakKrs.java
@@ -11,6 +11,8 @@ public class IfCetakKrs {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan bayar UKT terlebih dahulu");
         }
 
         sc.close();
